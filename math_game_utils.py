9f402619afd71041b761409be79cb90b6b3e785c
Valentin Fechter <vafechte@k1r1p11.42mulhouse.fr>
2026-05-07 19:26:52 +0200

    add normalize, length method to Vec2, and operator truediv, add, mul, sub

--- math_game_utils.py
+++ math_game_utils.py
@@ -3,9 +3,32 @@ import math
 
 
 class Vec2:
-    def __init__(self, x: int | float, y: int | float):
+    def __init__(self, x: float, y: float):
         self.x = x
         self.y = y
 
     def distance_to(self, other: Vec2) -> float:
         return math.sqrt((other.x - self.x) ** 2 + (other.y - self.y) ** 2)
+
+    def normalize(self) -> Vec2:
+        return self.__truediv__(self.length())
+
+    def length(self) -> float:
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+
+    def __add__(self, other: Vec2) -> Vec2:
+        return Vec2(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: Vec2) -> Vec2:
+        return Vec2(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, scalar: float) -> Vec2:
+        return Vec2(self.x * scalar, self.y * scalar)
+
+    def __truediv__(self, scalar: float) -> Vec2:
+        """__truediv__ can raise a ZeroDivisionError"""
+
+        return Vec2(self.x / scalar, self.y / scalar)
+
+    def __str__(self) -> str:
+        return f"Vec2(x: {self.x}, y: {self.y})"
--- test.py
+++ test.py
@@ -1,7 +1,8 @@
-from arcade import Vec2
+from math_game_utils import Vec2
 
+t = Vec2(100.0, 200.0)
+t2 = Vec2(100.0, 200.0)
 
-t = Vec2(1.0, 1.0)
+t3 = t / 2.0
 
-
-print(t)
+print(t3)
