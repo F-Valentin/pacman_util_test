551908a3255f27592ed5f9ed983b2362367b8be1
Valentin Fechter <vafechte@k0r1p6.42mulhouse.fr>
2026-05-08 17:00:04 +0200

    add enemy

--- enemy.py
+++ enemy.py
@@ -1,5 +1,19 @@
-from abc import ABC
+import arcade
+from arcade.types import PathOrTexture
 
+from algorithm_strategy import PathfindingStrategy
 
-class Enemy(ABC):
-    pass
+class Ghost(arcade.Sprite):
+    def __init__(self, path_to_animation: str, strategy: PathfindingStrategy) -> None:
+        super().__init__()
+        self._sprite_list: arcade.SpriteList = arcade.SpriteList()
+        self._animation = arcade.load_animated_gif(path_to_animation)
+        self._sprite_list.append(self._animation)
+        # self.speed
+        self._strategy = strategy
+    
+
+    @property
+    def strategy(self) -> PathfindingStrategy:
+        return self._strategy
+    
