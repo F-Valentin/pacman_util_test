de3f2825e35eb7e47dd0b51120f3f98d1ccb2e26
Valentin Fechter <vafechte@k1r3p11.42mulhouse.fr>
2026-05-13 15:03:22 +0200

    add oberserver logic to player and ghsot

--- player.py
+++ player.py
@@ -1,3 +1,33 @@
+from subsriber import IPlayerSubscriber
+
 class Player:
     def __init__(self) -> None:
-        pass
+        self._subscribers: list[IPlayerSubscriber] = []
+
+    @property
+    def subscribers(self) -> list[IPlayerSubscriber]:
+        return self._subscribers
+
+    def add_subscriber(self, subscriber: IPlayerSubscriber) -> None:
+        self._subscribers.append(subscriber) 
+
+    def remove_subscriber(self, subscriber: IPlayerSubscriber) -> None:
+        self._subscribers.remove(subscriber) 
+    
+    def die(self) -> None:
+        for subscriber in self._subscribers:
+            subscriber.on_player_death()
+    
+    def eat_pacgum(self) -> None:
+        for subscriber in self._subscribers:
+            subscriber.on_player_ate_super_pacgum()
+    
+    def level_completed(self) -> None:
+        for subscriber in self._subscribers:
+            subscriber.on_player_completed_level()
+
+
+player = Player()
+
+player.die()
+player.level_completed()
