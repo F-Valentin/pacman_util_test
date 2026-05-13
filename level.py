e5f278cc7292344301f0207f9976f950f0a856ef
val <val@vals-MacBook-Air.local>
2026-05-13 23:40:22 +0200

    add ViewManager

--- level.py
+++ level.py
@@ -1,4 +1,3 @@
-from enum import IntEnum
 import arcade
 
 from abc import ABC, abstractmethod
@@ -7,11 +6,6 @@ from player import Player
 from enemy import Ghost
 from maze import Maze
 from cell import Cell
-
-class LevelDifficulty(IntEnum):
-    EASY = 0
-    MEDIUM = 1
-    HARD = 2
 
 class LevelSwitcher(ABC):
     @abstractmethod
@@ -25,30 +19,9 @@ class Level(arcade.View):
         self._maze = maze
         self._level_switcher = level_switcher
 
-# simple idea (not finished)
 class LevelFactory:
     def __init__(self) -> None:
         pass
-
-    @staticmethod
-    def create_level(level_size: tuple[int, int], level_difficulty: LevelDifficulty) -> Level:
-        player = Player()
-        ghosts = []
-        maze_generator = MazeGenerator()
-        maze: list[list[Cell]] = []
-
-        for (y, row) in enumerate(maze_generator.maze):
-            maze.append([])
-            for (x, col) in enumerate(row):
-                maze[y].append(Cell(x, y, col, (15, 15), False))
-        ...
-    
-    @staticmethod
-    def create_levels(nb_of_levels: int, levels_size: list[tuple[int, int]], levels_difficulty: list[LevelDifficulty]):
-        levels: list[Level] = []
-        for i in range(nb_of_levels):
-            levels.append(LevelFactory.create_level(levels_size[i], levels_difficulty[i]))
-        ...
 
 class LevelManager(LevelSwitcher):
     def __init__(self, window: arcade.Window, levels: list[Level]) -> None:
---
+++ view_manager.py
@@ -0,0 +1,30 @@
+import arcade
+import logging
+
+class ViewManager:
+    def __init__(self, window: arcade.Window) -> None:
+        self._window = window
+        self._views: dict[str, arcade.View] = {}
+
+    def add_view(self, view_name: str, view: arcade.View):
+        self._views[view_name] = view
+
+    def switch_view(self, view_name: str):
+        try:
+            view = self._views[view_name]
+        except KeyError as e:
+            raise e
+        self._window.show_view(view)
+
+
+
+    def switch_view(self, view_name: str) -> bool:
+        try:
+            view = self._views[view_name]
+        except KeyError as e:
+            logging.warning(e)
+            return False
+
+        self._window.show_view(view)
+
+        return True
