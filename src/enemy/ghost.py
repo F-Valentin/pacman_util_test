9706f1d8c9774ba6e88bd47239fc58aed09acadc
Mateo Dos Santos Joaquim <matdos-s@k1r1p7.42mulhouse.fr>
2026-05-17 17:45:12 +0200

    remouve arcade.Sprite  heritence from gost

--- src/enemy/ghost.py
+++ src/enemy/ghost.py
@@ -1,15 +1,31 @@
 import arcade
-from algorithms.algorithm_strategy import PathfindingStrategy
+from arcade import SpriteList
 from subscriber import IGhostSubscriber
+from algorithms.algorithm_strategy import PathfindingStrategy
 
 
-class Ghost(arcade.Sprite):
+class Ghost():
+
     def __init__(self, path_to_sprite: str,
                  strategy: PathfindingStrategy) -> None:
-        super().__init__(path_to_sprite)
+        self.sprite: arcade.Sprite = arcade.Sprite(path_to_sprite)
+        self.sprite.scale = 0.06
+
+        self._sprite_list: SpriteList[arcade.Sprite] = SpriteList()
+        self._sprite_list.append(self.sprite)
 
         self._strategy = strategy
         self._subscribers: list[IGhostSubscriber] = []
+
+    def set_position(self, x: int, y: int) -> None:
+        self.sprite.center_x = x
+        self.sprite.center_y = y
+
+    def update(self) -> None:
+        self._sprite_list.update()
+
+    def draw(self) -> None:
+        self._sprite_list.draw()
 
     @property
     def strategy(self) -> PathfindingStrategy:
